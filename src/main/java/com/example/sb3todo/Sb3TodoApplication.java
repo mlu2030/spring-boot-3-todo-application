058cd7a8e7aff9f515a3bf2f1aaef21180125f77
mlu2030 <80307778+mlu2030@users.noreply.github.com>
2023-11-06 21:42:30 -0500

    Spring Boot 3 application, having started from that generated by the Spring Initializr

---
+++ src/main/java/com/example/sb3todo/Sb3TodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb3todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb3TodoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/sb3todo/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.sb3todo.controllers;
+
+import com.example.sb3todo.services.TodoItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private TodoItemService todoItemService;
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemService.getAll());
+        return modelAndView;
+    }
+}
